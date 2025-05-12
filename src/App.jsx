740d409149a245c0cd51b82fb1202d454f899476
hj <hj@hjs-MacBook-Pro.local>
2025-05-12 15:01:26 +0900

    완성

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/quiz/:nickname" element={<Quiz />}></Route>
         <Route path="/results/:nickname" element={<Results />}></Route>
       </Routes>
